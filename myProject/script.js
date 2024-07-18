b9249051615d9b42c3ed67b227f32f2b3e68ca58
sruthemurugan <136948240+sruthemurugan@users.noreply.github.com>
2024-07-18 20:54:28 +0530

    Add files via upload

---
+++ myProject/script.js
@@ -0,0 +1,54 @@
+var settingsmenu = document.querySelector(".settings-menu");
+var darkBtn = document.getElementById("dark-btn");
+
+function settingsMenuToggle(){
+    settingsmenu.classList.toggle("settings-menu-height");
+
+}
+darkBtn.onclick = function(){
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+
+    if(localStorage.getItem("theme") == "light"){
+        localStorage.setItem("theme", "dark");
+    }
+    else{
+        localStorage.setItem("theme", "light");
+    }    
+}
+
+if(localStorage.getItem("theme") == "light"){
+    darkBtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+}
+else if(localStorage.getItem("theme") == "dark"){
+    darkBtn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme");
+}
+else{
+    localStorage.setItem("theme", "light");
+
+}
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function auth(event) {
+    event.preventDefault();
+
+    var email = document.getElementById("Email").value;
+    var password = document.getElementById("password").value;
+
+    if (email === "james008@gmail.com" && password === "james008") {
+         window.location.replace("/frontpage.html");
+    } else {
+        alert("Invalid information");
+        return;
+    }
+}
